a653d23eef2ce9dbcaf33d9dc291d5b6a981c2ce
Foalz <pjcontreras20202@gmail.com>
2025-07-18 14:59:42 +0000

    [ADD] hr_birthday module: module structure scaffold

---
+++ src/third-party-addons/hr_birthday/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'HR Birthday Management',
+    'version': '17.0.1.0.0',
+    'summary': '',
+    'description': '',
+    'author': 'Pedro Contreras',
+    'website': 'https://www.tuempresa.com',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'data': [
+        'views/account_move.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
