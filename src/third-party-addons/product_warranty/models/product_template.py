8dcfbfca3d432959375482a9dd64067a33469bc3
Foalz <pjcontreras20202@gmail.com>
2025-07-19 19:42:47 -0400

    [FEAT] Validating if a product has variants with inverse and compute

--- src/third-party-addons/product_warranty/models/product_template.py
+++ src/third-party-addons/product_warranty/models/product_template.py
@@ -5,5 +5,23 @@ from odoo import models, fields, api, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    warranty_period = fields.Integer(string='Warranty Period', store=True,)
-    
+    warranty_period = fields.Integer(
+        string='Warranty Period', 
+        store=True,
+        compute='_compute_warranty_period',
+        inverse='_set_warranty_period',
+        help='Warranty period in months. If the product has multiple variants, this field will be empty.',
+    )
+
+    @api.depends('product_variant_ids.warranty_period')
+    def _compute_warranty_period(self):
+        for template in self:
+            if template.product_variant_count == 1:
+                template.warranty_period = template.product_variant_ids.warranty_period
+            else:
+                template.warranty_period = False
+
+    def _set_warranty_period(self):
+        for template in self:
+            if template.product_variant_count == 1:
+                template.product_variant_ids.warranty_period = template.warranty_period
