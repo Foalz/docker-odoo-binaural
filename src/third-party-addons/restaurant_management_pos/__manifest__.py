26250a8835535975adbe1266cdce84969dc8bed9
Foalz <pjcontreras20202@gmail.com>
2025-07-20 01:43:27 -0400

    [ADD] restaurant management module initial structure

---
+++ src/third-party-addons/restaurant_management_pos/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Restaurant Management POS',
+    'version': '17.0.1.0.0',
+    'author': 'Pedro Contreras',
+    'website': 'https://www.tuempresa.com',
+    'category': 'Sales',
+    'depends': ['point_of_sale'],
+    'data': [
+
+    ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
