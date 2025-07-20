c8cac9b46dba7d247aaf80a6a7ec76cd8b0de758
Foalz <pjcontreras20202@gmail.com>
2025-07-19 22:32:54 -0400

    [ADD] Settings to stock module

---
+++ src/third-party-addons/stock_picking_quality/models/res_config_settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import _, api, fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    stock_picking_quality = fields.Boolean(
+        string='Enable Stock Picking Quality',
+        help='Enable quality checks for stock picking operations.'
+    )
+    
