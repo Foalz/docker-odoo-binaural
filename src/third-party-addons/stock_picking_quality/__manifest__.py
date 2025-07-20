9d87c9b42193472af4ae0a7714d483fb0dab54f4
Foalz <pjcontreras20202@gmail.com>
2025-07-19 21:35:20 -0400

    [ADD] Stock Picking warranty module initial structure

---
+++ src/third-party-addons/stock_picking_quality/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Stock Picking Quality',
+    'version': '17.0.1.0.0',
+    'author': 'Pedro Contreras',
+    'website': 'https://www.tuempresa.com',
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'data': [
+        
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
