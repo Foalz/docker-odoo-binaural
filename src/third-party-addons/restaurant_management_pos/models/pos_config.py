dbec44d96dc0ccaf91c1eeeb68ad8905d7c65d53
Foalz <pjcontreras20202@gmail.com>
2025-07-20 21:19:12 -0400

    [REFINE] Implement custom logic in POS UI to check for active tables before opening

---
+++ src/third-party-addons/restaurant_management_pos/models/pos_config.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, _
+from odoo.exceptions import UserError
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    def open_ui(self):
+        """Override to add custom logic when opening the POS UI."""
+        table_ids = self.env['pos.tables'].search([('active', '=', True)])
+        if not table_ids:
+            raise UserError(_("No active tables found. Please create and activate tables before starting the POS."))
+        super(PosConfig, self).open_ui()
