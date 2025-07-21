3aceacaa64bf83fd3018083d147d276079341b3b
Foalz <pjcontreras20202@gmail.com>
2025-07-20 20:51:17 -0400

    [REFINE] Enhance Order model with table and partner management methods

--- src/third-party-addons/restaurant_management_pos/static/src/app/store/models.js
+++ src/third-party-addons/restaurant_management_pos/static/src/app/store/models.js
@@ -5,6 +5,27 @@ import { patch } from "@web/core/utils/patch";
 import { PosCollection, Order, Product } from "@point_of_sale/app/store/models";
 
 patch(Order.prototype, {
+    setup(_defaultObj, options) {
+        super.setup(_defaultObj, options);
+        this.table = null; // Initialize table to null
+    },
+    set_table(table) {
+        this.assert_editable();
+        this.table = table;
+        this.updatePricelistAndFiscalPosition(table);
+    },
+    get_table() {
+        return this.table;
+    },
+    set_partner(partner) {
+        console.log("Setting partner:", partner);
+        this.assert_editable();
+        this.partner = partner;
+        this.updatePricelistAndFiscalPosition(partner);
+    },
+    get_partner() {
+        return this.partner;
+    },
     init_from_JSON(json) {
         super.init_from_JSON(json);
         // Initialize screen data if not present
