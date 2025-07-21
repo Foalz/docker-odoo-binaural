dc60c65ce3d4b81c8f080a9f8777b17302cb4a4a
Foalz <pjcontreras20202@gmail.com>
2025-07-20 20:15:24 -0400

    [REFINE] Remove getStatus method from TicketScreen prototype to streamline code

--- src/third-party-addons/restaurant_management_pos/static/src/app/screens/ticket_screen/ticket_screen.js
+++ src/third-party-addons/restaurant_management_pos/static/src/app/screens/ticket_screen/ticket_screen.js
@@ -4,18 +4,6 @@ import { patch } from "@web/core/utils/patch";
 import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_screen";
 
 patch(TicketScreen.prototype, {
-    getStatus(order) {
-        if (order.locked) {
-            return order.state === "invoiced" ? _t("Invoiced") : _t("Paid");
-        } else {
-            const screen = order.get_screen_data();
-            console.log(screen);
-            console.log(order);
-            console.log(this._getOrderStates().get(this._getScreenToStatusMap()[screen.name]));
-        
-            return this._getOrderStates().get(this._getScreenToStatusMap()[screen.name]).text;
-        }
-    },
     _getScreenToStatusMap() {
         return {
             ProductScreen: "ONGOING",
