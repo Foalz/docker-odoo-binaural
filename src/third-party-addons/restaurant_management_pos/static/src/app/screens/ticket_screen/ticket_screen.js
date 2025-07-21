561a62b3b9a45f4413f74c8186c394f0b0cf80e4
Foalz <pjcontreras20202@gmail.com>
2025-07-20 20:09:08 -0400

    [REFINE] Rearrange logging statements in getStatus method for better clarity

--- src/third-party-addons/restaurant_management_pos/static/src/app/screens/ticket_screen/ticket_screen.js
+++ src/third-party-addons/restaurant_management_pos/static/src/app/screens/ticket_screen/ticket_screen.js
@@ -5,12 +5,12 @@ import { TicketScreen } from "@point_of_sale/app/screens/ticket_screen/ticket_sc
 
 patch(TicketScreen.prototype, {
     getStatus(order) {
-        console.log(order);
-        console.log(this._getOrderStates().get(this._getScreenToStatusMap()[screen.name]));
         if (order.locked) {
             return order.state === "invoiced" ? _t("Invoiced") : _t("Paid");
         } else {
             const screen = order.get_screen_data();
+            console.log(order);
+            console.log(this._getOrderStates().get(this._getScreenToStatusMap()[screen.name]));
             return this._getOrderStates().get(this._getScreenToStatusMap()[screen.name]).text;
         }
     }
