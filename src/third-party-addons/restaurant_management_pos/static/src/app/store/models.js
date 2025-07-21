2a25242458cbb5976623502f9e018af3b2b3defb
Foalz <pjcontreras20202@gmail.com>
2025-07-20 22:31:49 -0400

    [REFINE] Implement export_as_JSON method in Order model for enhanced order data serialization

--- src/third-party-addons/restaurant_management_pos/static/src/app/store/models.js
+++ src/third-party-addons/restaurant_management_pos/static/src/app/store/models.js
@@ -30,5 +30,49 @@ patch(Order.prototype, {
         super.init_from_JSON(json);
         // Initialize screen data if not present
         console.log("Initializing order from JSON:", json);
+    },
+    export_as_JSON() {
+        var orderLines, paymentLines;
+        orderLines = [];
+        this.orderlines.forEach((item) => {
+            return orderLines.push([0, 0, item.export_as_JSON()]);
+        });
+        paymentLines = [];
+        this.paymentlines.forEach((item) => {
+            const itemAsJson = item.export_as_JSON();
+            if (itemAsJson) {
+                return paymentLines.push([0, 0, itemAsJson]);
+            }
+        });
+        var json = {
+            name: this.get_name(),
+            amount_paid: this.get_total_paid() - this.get_change(),
+            amount_total: this.get_total_with_tax(),
+            amount_tax: this.get_total_tax(),
+            amount_return: this.get_change(),
+            lines: orderLines,
+            statement_ids: paymentLines,
+            pos_session_id: this.pos_session_id,
+            pricelist_id: this.pricelist ? this.pricelist.id : false,
+            partner_id: this.get_partner() ? this.get_partner().id : false,
+            table_id: this.get_table() ? this.get_table().id : false,
+            user_id: this.pos.user.id,
+            uid: this.uid,
+            sequence_number: this.sequence_number,
+            date_order: serializeDateTime(this.date_order),
+            fiscal_position_id: this.fiscal_position ? this.fiscal_position.id : false,
+            server_id: this.server_id ? this.server_id : false,
+            to_invoice: this.to_invoice ? this.to_invoice : false,
+            shipping_date: this.shippingDate ? this.shippingDate : false,
+            is_tipped: this.is_tipped || false,
+            tip_amount: this.tip_amount || 0,
+            access_token: this.access_token || "",
+            last_order_preparation_change: JSON.stringify(this.lastOrderPrepaChange),
+            ticket_code: this.ticketCode || "",
+        };
+        if (!this.is_paid && this.user_id) {
+            json.user_id = this.user_id;
+        }
+        return json;
     }
 });
